72a60b02959aa1d78125525fe5b4266ad3398e31
BreannaBrzezinski <88799188+BreannaBrzezinski@users.noreply.github.com>
2021-08-11 12:23:08 -0700

    Add files via upload

---
+++ exercise-2/js/app.js
@@ -0,0 +1,103 @@
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
+
+var createNewTaskElement = function(taskString, arr) {
+    var listItem = document.createElement("li");
+    var checkBox = document.createElement("input");
+    var label = document.createElement("label");
+    var editInput = document.createElement("input");
+    var editButton = document.createElement("button");
+    var deleteButton = document.createElement("button");
+
+            checkBox.type = "checkbox";
+            editInput.type = "text";
+            editButton.innerText = "Edit";
+            editButton.className = "edit";
+            deleteButton.innerText = "Delete";
+            deleteButton.className = "delete";
+            label.innerText = taskString;
+            
+            listItem.appendChild(checkBox);
+            listItem.appendChild(label);
+            listItem.appendChild(editInput);
+            listItem.appendChild(editButton);
+            listItem.appendChild(deleteButton);
+            
+            return listItem;
+        };
+          
+
+var addTask = function() {
+    if (taskInput.value !== "") {
+        var listItem = createNewTaskElement(taskInput.value);
+        incompleteTasksHolder.appendChild(listItem);
+        bindTaskEvents(listItem, taskCompleted);
+        taskInput.value = "";
+    }
+    else {
+        alert("please enter an item");
+    }
+}
+
+var editTask = function () {
+    var listItem = this.parentNode;
+    var editInput = listItem.querySelectorAll("input[type=text")[0];
+    var label = listItem.querySelector("label");
+    var button = listItem.getElementsByTagName("button")[0];
+
+    var containsClass = listItem.classList.contains("editMode");
+      if (containsClass) {
+          label.innerText = editInput.value
+          button.innerText = "Edit";
+      } else {
+          editInput.value = label.innerText
+          button.innerText = "Save";
+      }
+    
+    listItem.classList.toggle("editMode");
+};
+
+var deleteTask = function (el) {
+    var listItem = this.parentNode;
+    var ul = listItem.parentNode;
+    ul.removeChild(listItem);
+};
+
+var taskCompleted = function (el) {
+    var listItem = this.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+};
+
+var taskIncomplete = function() {
+    var listItem = this.parentNode;
+    incompleteTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+};
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler, cb) {
+    var checkBox = taskListItem.querySelectorAll("input[type=checkbox]")[0];
+    var editButton = taskListItem.querySelectorAll("button.edit")[0];
+    var deleteButton = taskListItem.querySelectorAll("button.delete")[0];
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+};
+
+addButton.addEventListener("click", addTask);
+
+for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
+    bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
+    bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
+
+localStorage.setItem("completedTasksHolder", JSON.stringify(completedTasksHolder));
+localStorage.setItem("incompleteTasksHolder", JSON.stringify(incompleteTasksHolder));
+          
+localStorage.getItem("completedTasksHolder");
+localStorage.getItem("incompleteTasksHolder");
